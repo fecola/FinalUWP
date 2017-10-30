9ec663eec96402d68961aec9af16840421170390
Danilo de Oliveira <danilo.t0458.tirrell@xpi.com.br>
2017-10-30 14:20:53 -0200

    Model Usuário e Site e página de Sobre

---
+++ MyFavoriteWeb/Models/Site.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFavoriteWeb.Models
+{
+    public class Site
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Url { get; set; }
+        public bool StatusLido { get; set; }
+    }
+}
---
+++ MyFavoriteWeb/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace MyFavoriteWeb.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public List<Site> Sites { get; set; }
+    }
+}
