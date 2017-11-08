aa4307ecfe0433d31c0d6f9102bb91e12f276ff4
Danilo Oliveira <danilotecoliveira@yahoo.com.br>
2017-11-08 00:53:59 -0200

    Implementação do Menu, frame e funcionalidades do módulo de WebView

--- MyFavoriteWeb/MainPage.xaml.cs
+++ MyFavoriteWeb/MainPage.xaml.cs
@@ -1,30 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using MyFavoriteWeb.ViewModels;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace MyFavoriteWeb
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        public MainPageViewModel ViewModel { get; } = new MainPageViewModel();
+
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            ViewModel.Initialize();
+        }
+
+        private void WebView_Click(object sender, RoutedEventArgs e)
+        {
+            frameNavigation.Navigate(typeof(Views.NavegadorWebView));
+        }
+
+        private void ListaSites_Click(object sender, RoutedEventArgs e)
+        {
+            frameNavigation.Navigate(typeof(Views.ListaSitesView));
+        }
+
+        private void Cadastro_Click(object sender, RoutedEventArgs e)
+        {
+            frameNavigation.Navigate(typeof(Views.CadastroView));
+        }
+
+        private void Sobre_Click(object sender, RoutedEventArgs e)
+        {
+            frameNavigation.Navigate(typeof(Views.SobreView));
+        }
+
+        private void Configuracao_Click(object sender, RoutedEventArgs e)
+        {
+            frameNavigation.Navigate(typeof(Views.SobreView));
+        }
+
+        private void Sair_Click(object sender, RoutedEventArgs e)
+        {
+            //
         }
     }
 }
---
+++ MyFavoriteWeb/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using MyFavoriteWeb.Abstracts;
+using System.Threading.Tasks;
+
+namespace MyFavoriteWeb.ViewModels
+{
+    public class MainPageViewModel : NotifyableClass
+    {
+        public void Initialize()
+        {
+        
+        }
+
+        private bool _isSplitViewOpen;
+
+        public bool IsSplitViewOpen
+        {
+            get { return _isSplitViewOpen; }
+            set { Set(ref _isSplitViewOpen, value); }
+        }
+
+        public void HamburguerButton_Click()
+        {
+            IsSplitViewOpen = !IsSplitViewOpen;
+        }
+
+        public void WebView_Click()
+        {
+            
+        }
+    }
+}
